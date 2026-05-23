18ac70ec169a5bbf99e82fb81e65f4bedb677057
Yiwei Gong <imwithye@gmail.com>
2026-05-23 18:08:47 +0800

    feat: add react native implementation

--- app.tsx
+++ app.tsx
@@ -1,13 +1,45 @@
-import { ScrollView, Text } from "react-native";
+import { useState } from "react";
+import {
+  ScrollView,
+  type StyleProp,
+  Text,
+  TextInput,
+  View,
+  type ViewStyle,
+} from "react-native";
+
+const TextInputStyle: StyleProp<ViewStyle> = {
+  borderWidth: 1,
+  borderRadius: 4,
+  padding: 4,
+};
 
 export default function App() {
+  const [rnText, setRNText] = useState("");
+
   return (
     <ScrollView
       style={{
-        padding: 20,
+        width: "100%",
+        height: "100%",
+        paddingVertical: 80,
+        paddingHorizontal: 40,
       }}
     >
-      <Text>Hello World!</Text>
+      <View style={{ gap: 16 }}>
+        <Text style={{ fontSize: 24, fontWeight: "bold" }}>
+          Text Input Demo
+        </Text>
+        <View style={{ gap: 8 }}>
+          <Text>React Native</Text>
+          <TextInput
+            style={TextInputStyle}
+            value={rnText}
+            onChangeText={setRNText}
+          />
+          <Text>State: {rnText}</Text>
+        </View>
+      </View>
     </ScrollView>
   );
 }
